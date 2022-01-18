732a9b7e174474e38a7e369619f487b7061cf196
Eric White <47019383+edwhite12@users.noreply.github.com>
2022-01-18 11:19:15 -0600

    Update Classify_LAVEGMod_output_to_VCT_oneyear.py

--- Classify_LAVEGMod_output_to_VCT_oneyear.py
+++ Classify_LAVEGMod_output_to_VCT_oneyear.py
@@ -39,7 +39,7 @@ LVMout = LVMout[LVMout[:, 0].argsort()]
 sp_names = np.genfromtxt(LVMout_file_excon,skip_header=371, skip_footer = 173898, delimiter=',', dtype='str')
 sp_names = (np.char.strip(sp_names,'"'))
 
-LVMgrid = np.genfromtxt(LVMout_file_excon,skip_header=6, skip_footer = 173899, delimiter=',', dtype=int) # skip the top portion of the asc+ files and only read in the column data starting on line 623
+LVMgrid = np.genfromtxt(LVMout_file_excon,skip_header=6, skip_footer = 173899, delimiter=' ', dtype=int) # skip the top portion of the asc+ files and only read in the column data starting on line 623
 
 cellxspecies = np.genfromtxt(species_lookup,delimiter=',',dtype='float') 
 SOM_species = np.genfromtxt(SOM_lookup,delimiter=',',skip_header=1,dtype='str') 
