41de2e1ac4661e22b54e40adeb07643fc7da6885
Maddie Foster-Martinez <mrfoster@uno.edu>
2024-04-03 15:37:40 -0500

    Create LVM_reset_coverages.f90
    first stab at reset coverages subroutine

---
+++ LVM_reset_coverages.f90
@@ -0,0 +1,30 @@
+subroutine reset_coverages
+    ! global arrays updated by subroutine:
+    !      bare_old
+    !      bare_new
+    !      dead_Flt
+    !
+    ! This subroutine converts the previous model year's new bareground (bare_new) to old bareground (bare_old) and removes the whole Morph pixel portion of the dead flotant (dead_Flt). 
+    
+   
+    use params
+    implicit none
+
+    ! local variables --> none
+  
+    ! Add all new bareground to old bareground 
+    bare_old(:,2) = bare_old(:,1) + bare_new(:,1)
+
+    ! Dump previous values of old bareground and move new values to column 1 for the next processing step
+    bare_old(:,1) = bare_old(:,2)
+    bare_old(:,2) = 0
+
+    ! Remove the whole Morph pixel portion of the dead flotant 
+    dead_Flt(:,2) = dead_Flt(:,1) - (dead_Flt(:,1)/((1/256)*(1/256)))   ! 256 is the number of Morph 30 m pixels in each 480 m grid cell 
+
+    ! Dump previous values of dead flotant and move new values to column 1 for the next processing step
+    dead_Flt(:,1) = dead_Flt(:,2)
+    dead_Flt(:,2) = 0
+
+
+end reset_coverages
