c76e2001940b4cd4604278125729d2723fd8ac8a
Eric White <eric.davidson.white@gmail.com>
2026-08-25 16:33:17 -0500

    add print statement for disperal likelihood debug

--- LVM_calc_dispersal_coverage.f90
+++ LVM_calc_dispersal_coverage.f90
@@ -33,6 +33,12 @@ subroutine calc_dispersal_coverage
 
     disp_cov = 0.0                                                              ! initialize array before first use
     
+    
+    
+    open(unit=666, file='veg/'//trim(adjustL(fnc_tag))//'_'//trim(adjustL(fileflag))//'_'//year//'_V_veg_high_disp_est_debug.csv')
+    write(666,'A') 'grid,numerator,denominator,sal_av_yr,wlv_smr'    
+
+
     do ig=1,ngrid
         do ic=1,ncov
             disp_cov(ig,ic)= coverages(ig,ic)                                   ! dispersal coverage for the coverages within the central cell 
@@ -66,7 +72,47 @@ subroutine calc_dispersal_coverage
                 disp_cov(ig,ic) = disp_cov(ig,ic) + (numerator/denominator) ! add to it the dispersal coverage for the coverages in the surrounding cells (nearest neighbors)
             endif 
         end do   
+
+        write(666,3333) ig,numerator,denominator,sal_av_yr(grid_comp(ig)),wlv_smr(grid_comp(ig))
     end do 
 
+    close(666)
 
+
+    open(unit=666, file='veg/'//trim(adjustL(fnc_tag))//'_'//trim(adjustL(fileflag))//'_'//year//'_V_veg_high_disp_est_exp_lkd.csv'
+    write(666,2222) 'exp_lkd_total',trim(adjustL(veg_coverage_file_header))
+    do ig = 1,ngrid
+        write(666,4444) ig,exp_lkd(ig,:),exp_lkd_total(ig)
+    end do
+    close(666)
+
+    open(unit=666, file='veg/'//trim(adjustL(fnc_tag))//'_'//trim(adjustL(fileflag))//'_'//year//'_V_veg_high_disp_est_est_p.csv'
+    write(666,'(A)') trim(adjustL(veg_coverage_file_header))
+    do ig = 1,ngrid
+        write(666,5555) ig,establish_P(ig,:)
+    end do
+    close(666)
+
+    open(unit=666, file='veg/'//trim(adjustL(fnc_tag))//'_'//trim(adjustL(fileflag))//'_'//year//'_V_veg_high_disp_est_mort_p.csv'
+    write(666,'(A)') trim(adjustL(veg_coverage_file_header))
+    do ig = 1,ngrid
+        write(666,5555) ig,mortality_P(ig,:)
+    end do
+    close(666)
+
+    open(unit=666, file='veg/'//trim(adjustL(fnc_tag))//'_'//trim(adjustL(fileflag))//'_'//year//'_V_veg_high_disp_est_disp_cov.csv'
+    write(666,'(A)') trim(adjustL(veg_coverage_file_header))
+    do ig = 1,ngrid
+        write(666,5555) ig,disp_cov(ig,:)
+    end do
+    close(666)
+
+
+
+2222    format(A,','A)
+3333    format(I0,',',F0.4,',',F0.4,',',F0.4,',',F0.4)
+4444    format(I0,',',F0.4,<ncov>(',',F0.4))
+5555    format(I0,',',<ncov>(',',F0.4))
+
+    return
 end 
